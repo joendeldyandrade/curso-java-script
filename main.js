251f64aab8a659f66381183783652f42d0b2a258
Joendel Dyandrade <joendelandrade@gmail.com>
2024-07-07 19:58:04 -0400

    numeros multiplicados

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-const names = [`Felipe`, `João`, `Julia`, 10, false];
-const João = names[1];
-console.log(João);
-names.push(`Pedro`);
-console.log(names);
+const number = [1, 2, 3, 4, 5];
 
-names.unshift("Fernanda");
-console.log(names);
-
-names.pop();
-names.pop();
-names.pop();
-
-names[3] = "Gustavo";
-
-console.log(names);
-
-const indexOfFelipe = names.indexOf("Felipe");
-const sortedNames = names.sort();
-console.log(names.length);
-console.log(sortedNames);
+const numberMultipliedByeTwo = number.map(function (number) {
+  return number * 2;
+});
+console.log(numberMultipliedByeTwo);
